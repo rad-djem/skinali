211e58619ff8f07c97f4507f8165412dc9175c9d
Kyrylo Harnichev <lap_gk@mail.ru>
2019-07-12 15:40:35 +0300

    Пофиксил баги

--- js/main.js
+++ js/main.js
@@ -26,7 +26,18 @@ $(window).load(function() {
 	 });
 	 $("a[href^='#']").click(function () {
 		 var _href = $(this).attr("href");
-		 $("html, body").animate({ scrollTop: $(_href).offset().top -230 + "px"});
+		 $("html, body").animate({ scrollTop: $(_href).offset().top -80 + "px"});
 		 return false;
 	 });
+	 $('input[type="tel"]').mask("+7 (999) 999-99-99");
+
+	//  Прогрузка Яндекс-map после скрола //
+	var reviews = $('.rivews');
+	var reviewsTop = reviews.offset().top;
+	$(window).bind('scroll', function () {
+		var windowTop = $(this).scrollTop();
+		if (windowTop > reviewsTop);
+		$('#map').html('<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ae3614ce175b2347758566a23ca1b200aaafe92abd478950c9a940c4c08b06c52&amp;width=100%25&amp;height=410&amp;lang=ru_RU&amp;scroll=false"></script>');
+		$(window).unbind('scroll');
+	});
 });
